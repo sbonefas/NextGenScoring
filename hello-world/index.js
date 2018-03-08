96856a5188731b3410afecb547dbe54e9d4d48cf
Nathaniel Gale <ngale@wisc.edu>
2018-03-08 17:16:59 -0600

    Add files via upload

---
+++ hello-world/index.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+
+function boot() {
+	win = new BrowserWindow();
+	win.loadURL(url.format({
+		pathname: 'index.html',
+		slashes: true
+	}))
+	
+}
+
+app.on('ready', boot);
