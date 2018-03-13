c69297454b96b6f34a55e79cb529e4aa0c259eb9
Nathaniel Gale <ngale@wisc.edu>
2018-03-12 22:54:58 -0500

    Add files via upload
    Added simple IPC call/response functionality between main process & renderer proecss (with input from index file)

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
-const {app, BrowserWindow} = require('electron');
-const url = require('url');
+console.log('main process loaded');
 
-function boot() {
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
+
+
+let win;
+
+function createWindow() {
 	win = new BrowserWindow();
 	win.loadURL(url.format({
-		pathname: 'index.html',
-		slashes: true
-	}))
-	
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file',
+		slashes: true		
+	}));
+
+
+	win.on('closed', () => {
+		win = null;
+	})
 }
 
-app.on('ready', boot);
+ipc.on('async-message', function(event){
+	event.sender.send('async-reply', 'Main process opened the error dialog');
+})
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (win === null) {
+		createWindow();
+	}	
+})
