ea81f6ab91eae7ecce0488b18b4cf163dda26916
Doug <dneu010@gmail.com>
2018-03-22 22:03:54 -0500

    Delete tutorial files

--- Tutorial Files/selenium_test.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlretrieve
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Firefox()
-driver.get("http://localhost:8080/#/")
-
-driver.find_element_by_name("skill").send_keys('F');
