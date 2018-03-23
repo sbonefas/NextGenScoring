2ca0c4d90143cfbf754731d41e16ef71088dba4d
Nate Gale <ngale@wisc.edu>
2018-03-22 22:01:27 -0500

    merged tests

---
+++ Tests/backend tests/backend_test.py
@@ -0,0 +1,12 @@
+from urllib.request import urlretrieve
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import unittest
+
+driver = webdriver.Firefox()
+driver.get("file:///C:/Projects/NextGenScoring/index.html")
+
+#driver.find_element_by_id("playbyplaybox").send_keys('F');
+
+
