518564c41942b6c3095748730aaf9b4563d94687
Damon Francisco <dfrancisco@wisc.edu>
2018-04-03 22:10:16 -0500

    Refactored test_shot_codes to be easier to re... -use. Test now tears down each time. Smaller tests were combined into a bigger one which have small helper methods for the smaller components. Fixed rebound errors from last time aside from "D". 3 expected failures and 1 unexpected error.

--- Tests/frontend tests/test_shot_codes.py
+++ Tests/frontend tests/test_shot_codes.py
@@ -14,26 +14,27 @@ def configure_index():
 class TestShotCodes(unittest.TestCase):
     driver = None
     score = 0
-    list = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    list = []
 	
     @classmethod
     def setUpClass(cls):
-        __class__.driver = webdriver.Firefox()
-        path = configure_index()
-        __class__.driver.get(path)
+        pass
 		
     @classmethod
     def tearDownClass(cls):
-	    #__class__.driver.quit()
         pass
 
     def setUp(self):
-        pass
+        __class__.driver = webdriver.Firefox()
+        path = configure_index()
+        __class__.driver.get(path)
+        __class__.score = 0
+        __class__.list = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 	
     def tearDown(self):
-        # Switches to V after all keycodes except R. All tests designed for home team to score
-        __class__.driver.find_element_by_id("app").send_keys("H")
-	     
+        __class__.driver.quit()
+	    #pass
+        
     """Helper/test sub-methods"""
     def handle_player_stats(self, index):
         self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/table/tbody/tr["+str(index)+"]/td[4]").text, str(__class__.list[0]))
@@ -52,19 +53,21 @@ class TestShotCodes(unittest.TestCase):
         Alert(__class__.driver).send_keys(keycode)
         Alert(__class__.driver).accept()
         __class__.driver.implicitly_wait(2)
-        # Assists not yet implemented
-        Alert(__class__.driver).accept()
 
         if keycode == "R":
             Alert(__class__.driver).send_keys(reb_key)
             Alert(__class__.driver).accept()
+            __class__.driver.implicitly_wait(5)
+            if reb_key == "D":
+                #TODO- Add additional parameter in above methods 
+                Alert(__class__.driver).send_keys("01")
+                Alert(__class__.driver).accept()
 
-    def play_by_play_always_same(self, index):
-        clock_time = __class__.driver.find_element_by_id("clockminutes").text + ":" + __class__.driver.find_element_by_id("clockseconds").text
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[1]").text, clock_time)
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "WISC")
-		
-    def add_score(self, player_no, keycode, reb_key, point_val):
+        else:
+            # Assists not yet implemented
+            Alert(__class__.driver).accept()
+
+    def update_stats(self, shot, player_no, keycode, reb_key, point_val):
         #id element doesn't actually matter
         __class__.driver.find_element_by_id("app").send_keys("J")
         # Give home team's player_no a keycode
@@ -74,97 +77,145 @@ class TestShotCodes(unittest.TestCase):
             __class__.score += point_val
         self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[1]/div[1]/div[2]/h2[2]").text, str(__class__.score))
 		# Ensure that player stats have been updated
-        __class__.list[1] += 1
+        if shot:
+            __class__.list[1] += 1
         if keycode == "G" or keycode == "Q" or keycode == "P":
             __class__.list[0] += 1
             __class__.list[8] += 2
         elif keycode == "Y":
             __class__.list[0] += 1
             __class__.list[2] += 1
-            __class__.list[8] += 1
+            __class__.list[8] += 3
         elif keycode == "R":
-            __class__.list[5] += 1
+            # TODO - Later accept any two digit player_no
+            if reb_key == player_no:
+                __class__.list[5] += 1
         else:
-            print("")
-    """Automatically called test methods"""
-    def test_make_2pt_fg_scoreboard_and_player_stats(self):
-        __class__.add_score(self, "01", "G", None, 2)
+            pass
+
+    def scoreboard_and_player_stats(self, shot, player_no, keycode, reb_key, point_val):
+        __class__.update_stats(self, shot, player_no, keycode, reb_key, point_val)
         __class__.handle_player_stats(self, 2)
         # Ensure that total team stats have been updated
         __class__.handle_player_stats(self, 12)
+        if keycode == "R":
+            # Check home/visitor CSS to determine current team is properly set
+            if reb_key == "01" or reb_key == "M" or reb_key == "DB":
+                self.assertEqual(__class__.driver.find_element_by_id("pshometeamname").value_of_css_property("color"), "rgb(255, 0, 0)")
+                self.assertEqual(__class__.driver.find_element_by_id("pshometeamname").value_of_css_property("text-decoration"), "underline")
+            else:
+                self.assertEqual(__class__.driver.find_element_by_id("pshometeamname").value_of_css_property("color"), "rgb(0, 0, 0)")
+                self.assertEqual(__class__.driver.find_element_by_id("pshometeamname").value_of_css_property("text-decoration"), "none")
+                self.assertEqual(__class__.driver.find_element_by_id("psvisitorteamname").value_of_css_property("color"), "rgb(255, 0, 0)")
+                self.assertEqual(__class__.driver.find_element_by_id("psvisitorteamname").value_of_css_property("text-decoration"), "underline")
+            
+            #Ensure that opposing team player #1's rebound was added
+            if reb_key == "D":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/table/tbody/tr[2]/td[9]").text, "1")
     
-    def test_make_2pt_fg_team_percentages(self):
-        # Ensure that field goal percentage has been updated
-        fg_percentage = 100 * (__class__.list[0] / __class__.list[1])	
-        self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[1]").text, "FG%: "+ str(fg_percentage) + " 3FG%: 0 FT%: 0")
+    def team_percentages(self, keycode):
+        fg_percentage = 100 * (__class__.list[0] / __class__.list[1])
+		# Added extra zero because in the Vue app there are 2 decimal points whereas in Python there's 1
+        if keycode == "P" or keycode == "Q" or keycode == "P" or keycode == "R":
+            self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[1]").text, "FG%: "+ str(fg_percentage)+str(0) +" 3FG%: 0 FT%: 0")
+        elif keycode == "Y":
+            #TODO - Next iteration will have a 3 attempted attribute to use 
+            three_pt_fg_percentage = 100.00	
+            self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[1]").text, "FG%: "+str(fg_percentage)+str(0) +" 3FG%: "+str(three_pt_fg_percentage)+str(0) +" FT%: 0")
+  
+    def play_by_play(self, index, keycode, reb_key, point_val):
+        clock_time = __class__.driver.find_element_by_id("clockminutes").text + ":" + __class__.driver.find_element_by_id("clockseconds").text
+        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[1]").text, clock_time)
+        if keycode != "R":
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "WISC")
+        
+        if keycode == "G" or keycode == "Q":
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Player_1 made a jump shot")
+        elif keycode == "P":
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Player_1 made a shot in the paint")
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[4]").text, str(point_val)+"-0")
+        elif keycode == "R":
+            #keycode - Offensive player
+            if reb_key == "01":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "WISC")
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Offensive rebound by Player_1")
+            elif reb_key == "M":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "WISC")
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Offensive Team Rebound")
+            elif reb_key == "B":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "WISC")
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Offensive Deadball")
+            elif reb_key == "D":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "VISITOR")
+                # pass player number if d as argument
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Defensive rebound by Player_1")
+            elif reb_key == "DM":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "VISITOR")
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Defensive Team Rebound")
+            elif reb_key == "DB":
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[2]").text, "VISITOR")
+                self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[3]").text, "Defensive Deadball")      
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index)+"]/td[4]").text, str(point_val)+"-0")
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index+1)+"]/td[3]").text, "Player_1 J -> R")
+            self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr["+str(index+1)+"]/td[4]").text, str(point_val)+"-0")
+
+    """Automatically called test methods"""
     
-    def test_make_2pt_fg_paint_zplay_by_play(self):
-		# Check that play-by-play has been updated
-        __class__.play_by_play_always_same(self, 2)
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[2]/td[3]").text, "Player_1 made a shot in the paint")
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[2]/td[4]").text, "2-0")
-    
-    def test_make_2pt_fg_scoreboard_and_player_stats_alt(self):
+    def test_make_2pt_fg(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "G", None, 2)
+		# Ensure that field goal percentage has been updated
+        __class__.team_percentages(self, "G")
+        __class__.play_by_play(self, 2, "G", None, 2)
+
+    def test_make_2pt_fg__alt(self):
         # Give home team's player 1 a made 2 pointer - alternate key binding
-        __class__.add_score(self, "01", "Q", None, 2)
-        __class__.handle_player_stats(self, 2)
-        __class__.handle_player_stats(self, 12)
+        __class__.scoreboard_and_player_stats(self, True, "01", "Q", None, 2)
+        __class__.team_percentages(self, "Q")
+        __class__.play_by_play(self, 2, "Q", None, 2)
 
-    def test_make_2pt_fg_zplay_by_play(self):
-        # Check that play-by-play has been updated
-        __class__.play_by_play_always_same(self, 2)
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[2]/td[3]").text, "Player_1 made a jump shot")
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[2]/td[4]").text, "6-0")
-        __class__.play_by_play_always_same(self, 3)
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[3]/td[3]").text, "Player_1 made a jump shot")
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[3]/td[4]").text, "4-0")
-        __class__.play_by_play_always_same(self, 4)
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[4]/td[3]").text, "Player_1 made a shot in the paint")
-        self.assertEqual(__class__.driver.find_element_by_xpath("/html/body/div/div[3]/table/tbody/tr[4]/td[4]").text, "2-0")
-	
-
-    def test_make_2pt_fg_paint_scoreboard_and_player_stats(self):
-        __class__.add_score(self, "01", "P", None, 2)
-        __class__.handle_player_stats(self, 2)
-        __class__.handle_player_stats(self, 12)
-
-    def test_make_2pt_fg_paint_team_percentages(self):
-        fg_percentage = 100 * (__class__.list[0] / __class__.list[1])	
-        self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[1]").text, "FG%: "+str(fg_percentage)+" 3FG%: 0 FT%: 0")
-        self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[3]").text, "paint: 2 offto: 0 2ndch: 0 fastb: 0")
-
-    def test_make_3pt_fg_scoreboard_and_player_stats(self):
-        __class__.add_score(self, "01", "Y", None, 3)
-        __class__.handle_player_stats(self, 2)
-        __class__.handle_player_stats(self, 12)
-
-    def test_make_3pt_fg_team_percentages(self):	
-        fg_percentage = 100 * (__class__.list[0] / __class__.list[1])
-        three_pt_fg_percentage = 100 * (__class__.list[2] / (__class__.list[1] - 3))		
-        self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[1]").text, "FG%: "+str(fg_percentage)+" 3FG%: "+str(three_pt_fg_percentage)+" FT%: 0")
-
-    '''    
-    def test_make_3pt_fg_play_by_play(self):
-
-
-    def test_miss_2pt_fg_scoreboard_and_player_stats(self):
-	    # Offensive rebound by player 1
-        #__class__.add_score(self, "01", "R", "01", 2)
-        #__class__.add_score(self, "01", "R", "M", 2)
-        #__class__.add_score(self, "01", "R", "B", 2) Another prompt needed
-        #__class__.add_score(self, "01", "R", "D", 2) Another prompt needed
-        #__class__.add_score(self, "01", "R", "DM", 2)
-        #__class__.add_score(self, "01", "R", "DB", 2)
-        __class__.handle_player_stats(self, 2)
-        __class__.handle_player_stats(self, 12)
+    def test_make_2pt_fg_paint(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "P", None, 2)
+        __class__.team_percentages(self, "P")
+        self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[3]").text, "paint: 1 offto: 0 2ndch: 0 fastb: 0")
+        __class__.play_by_play(self, 2, "P", None, 2)
     
-    def test_miss_2pt_fg_team_percentages(self):
-        fg_percentage = 100 * (__class__.list[0] / __class__.list[1])
-        three_pt_fg_percentage = 100 * (__class__.list[2] / (__class__.list[1] - 4))		
-        self.assertEqual(__class__.driver.find_element_by_xpath("//*[@id='ps-home']/div/p[1]").text, "FG%: "+str(fg_percentage)+" 3FG%: "+str(three_pt_fg_percentage)+" FT%: 0")
+    def test_make_3pt_fg(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "Y", None, 3)
+        __class__.team_percentages(self, "Y")
+        __class__.play_by_play(self, 2, "Y", None, 3)
+    
+ 
+    """ Missed shot followed by a rebound""" 
+    def test_miss_2pt_fg_reb_off_player(self):
+	    # Offensive rebound by player 1
+        __class__.scoreboard_and_player_stats(self, True, "01", "R", "01", 2)
+        __class__.team_percentages(self, "R")
+        __class__.play_by_play(self, 2, "R", "01", 0)
+        
+    def test_make_2pt_fg_reb_off_team_reb(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "R", "M", 2)
+        __class__.team_percentages(self, "R")
+        __class__.play_by_play(self, 2, "R", "M", 0)
 
-    def test_miss_2pt_fg_play_by_play(self):
-	    pass
-    '''
+    def test_make_2pt_fg_reb_off_deadball(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "R", "B", 2)
+        __class__.team_percentages(self, "R")
+        __class__.play_by_play(self, 2, "R", "B", 0)
+    
+    def test_make_2pt_fg_reb_def_player(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "R", "D", 2)
+        __class__.team_percentages(self, "R")
+        __class__.play_by_play(self, 2, "R", "D", 0)
+        
+    def test_make_2pt_fg_reb_def_team_reb(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "R", "DM", 2)
+        __class__.team_percentages(self, "R")
+        __class__.play_by_play(self, 2, "R", "DM", 0)  
+        
+    def test_make_2pt_fg_reb_def_deadball(self):
+        __class__.scoreboard_and_player_stats(self, True, "01", "R", "DB", 2)
+        __class__.team_percentages(self, "R")
+        __class__.play_by_play(self, 2, "R", "DB", 0)
+    
 if __name__ == '__main__':
     unittest.main()
