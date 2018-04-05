098999b1422b61cc2db12d2c7487c6b1ab961c66
sbonefas <sdbon5678@yahoo.com>
2018-04-05 18:05:54 -0500

    moved login to be on main menu not G

--- mainmenu.js
+++ mainmenu.js
@@ -1,3 +1,21 @@
+var TESTING_MODE = false;  // this signals that you are in testing mode and will disable the annoying password promt that happens every time you load the page. Set this to false when in production mode!
+
+window.onload = function () {
+  if(!TESTING_MODE) {
+    $(document.body).hide();
+   var password = "";
+   while(password != "123") {
+    var password = prompt("Please enter the password to access this site.");
+    if(password != "123") {
+      alert("Incorrect password");
+    }
+   }
+   if(password == "123") {
+      $(document.body).show();
+   }
+  }
+}
+
 var app = new Vue({
     el: '#menu',
     created() {
