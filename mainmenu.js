dd9acb319b177f3a2f6bca4565b870d6bc9c2582
sbonefas <sdbon5678@yahoo.com>
2018-04-05 21:58:01 -0500

    added login page

--- mainmenu.js
+++ mainmenu.js
@@ -1,20 +1,22 @@
-var TESTING_MODE = false;  // this signals that you are in testing mode and will disable the annoying password promt that happens every time you load the page. Set this to false when in production mode!
+//var TESTING_MODE = false;  // this signals that you are in testing mode and will disable the annoying password promt that happens every time you load the page. Set this to false when in production mode!
 
-window.onload = function () {
-  if(!TESTING_MODE) {
-    $(document.body).hide();
-   var password = "";
-   while(password != "123") {
-    var password = prompt("Please enter the password to access this site.");
-    if(password != "123") {
-      alert("Incorrect password");
-    }
-   }
-   if(password == "123") {
-      $(document.body).show();
-   }
-  }
-}
+//window.onload = function () {
+//  if(!TESTING_MODE) {
+//    $(document.body).hide();
+//   password = "";
+//   while(password != "123") {
+//    var password = prompt("Please enter the password to access this site.");
+//    if(password != "123") {
+//      alert("Incorrect password");
+//    }
+//   }
+//   if(password == "123") {
+//      $(document.body).show();
+//      TESTING_MODE = true;
+//      console.log(TESTING_MODE)
+//   }
+//  }
+//}
 
 var app = new Vue({
     el: '#menu',
@@ -24,8 +26,8 @@ var app = new Vue({
     methods: {
         keyevent(e) {
             console.log(e.keyCode);
-            if(e.keyCode == 72) {
-                window.alert("menu");
+            if(e.keyCode == 71) {
+                window.location = "./teams.html"
             }
         }
     }
