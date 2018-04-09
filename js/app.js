227edb303b7f4ed0cd8b8e17b256ec3a60b70434
sbonefas <sdbon5678@yahoo.com>
2018-04-08 23:02:45 -0500

    fixed help menu to not be an alert (it's a modal now)

--- js/app.js
+++ js/app.js
@@ -114,7 +114,26 @@ function startClock(startingTime) {
 }
 
 function help() {
-    window.alert(help_menu);
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    // Get the <span> element that closes the modal
+    var span = document.getElementById("closeModal");
+
+    // show modal
+    modal.style.display = "block";
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
 
 var app = new Vue({
