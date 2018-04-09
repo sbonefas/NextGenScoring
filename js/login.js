1d8d09c944f5105e5e2319c176a04857b152e4c1
sbonefas <sdbon5678@yahoo.com>
2018-04-08 20:57:38 -0500

    added css to login and now it gives error msg for invalid password

--- js/login.js
+++ js/login.js
@@ -1,27 +1,12 @@
-TESTING_MODE = false;  // this signals that you are in testing mode and will disable the annoying password prompt that happens every time you load the page. Set this to false when in production mode!
-
-//window.onload = function () {
-//  if(!TESTING_MODE) {
-////    $(document.body).hide();
-//   password = "";
-//   while(password != "123") {
-//    var password = prompt("Please enter the password to access this site.");
-//    if(password != "123") {
-//      alert("Incorrect password");
-//    }
-//   }
-//   if(password == "123") {
-////      $(document.body).show();
-//      window.location = "./mainmenu.html"
-//   }
-//  }
-//}
-
 function pswrdEntered() {
     var password = document.getElementById("pswrd").value;
     console.log(password)
     if(password == "123") {
         window.location = "./mainmenu.html"
+    }
+    else {
+        var x = document.getElementById("errormsg");
+        x.style.display = "block"
     }
 }
 
@@ -34,7 +19,7 @@ var app = new Vue({
         keyevent(e) {
             console.log(e.keyCode);
             if(e.keyCode == 13) {
-                app.pswrdEntered()
+                app.pswrdEntered();
             }
         },
         pswrdEntered() {
@@ -44,7 +29,8 @@ var app = new Vue({
                 window.location = "./mainmenu.html"
             }
             else {
-
+                var x = document.getElementById("errormsg");
+                x.style.display = "block"
             }
         }
     }
