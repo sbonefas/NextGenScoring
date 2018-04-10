8ea944288f64d5b9271c320154a58479bf5756d9
Doug <dneu010@gmail.com>
2018-04-10 17:59:22 -0500

    increased statement coverage with new test

---
+++ test/test_delete_file.js
@@ -0,0 +1,9 @@
+const drw = require('../data_read_write');
+const fs = require('fs');
+
+describe('new test file', function() {
+     it('should properly transform a string of stats to a 2D array', function() {
+         drw.test_delete_file("string");
+     });
+   });
+   
