f086577b547c042e6de6a03412bc25d9de350688
Noah Haselow <nhaselow@wisc.edu>
2018-04-11 23:10:32 -0500

    Added team read write with tests

--- team_read_write.js
+++ team_read_write.js
@@ -8,7 +8,7 @@
  * public functions: 												*
  * 		1. delete_file(file_name)									*
  *			- deletes the file with the given filename 				*
- *		2. edit_team_data_location_path(new_path)					*
+ *		2. edit_team_directory(new_path)							*
  *			- changes the directory that team files are stored		*
  *		3. create_team(file_name, content)							*
  *			- creates a team file and fills it with 				*
@@ -25,9 +25,10 @@
 
 
 const fs = require("fs");	//node.js filesystem
+const util = require("util");	//node.js utility module
 
-/** path to the folder where data is kept */
-var team_data_location_path = "data/";
+/** path to the folder where team data is kept */
+var team_directory = "team/";
 
 /** 
  * Returns the filepath of a file with a given name
@@ -36,7 +37,7 @@ var team_data_location_path = "data/";
  * @return Filepath of the file
  */
 function get_file_path(file_name) {
-	return team_data_location_path + file_name + '.txt';
+	return team_directory + file_name + '.txt';
 }
 
 exports.delete_file = function(file_name) {
@@ -51,9 +52,9 @@ exports.delete_file = function(file_name) {
  * @return True if path is successfully edited. False if the directory path
  * does not exist.
  */
-exports.edit_team_data_location_path = function(new_path) {
+exports.edit_team_directory = function(new_path) {
 	if(!fs.existsSync(new_path)) return false;
-	team_data_location_path = new_path;
+	team_directory = new_path;
 	return true;
 }
 
@@ -62,21 +63,29 @@ exports.edit_team_data_location_path = function(new_path) {
  *
  * @param file_name Name of the file
  * @param content Array representation of the team to write to file
+ * @throw error if file already exists
  */
 exports.create_team = function(file_name, content) {
-	//todo
-	return;
+	var file_path = get_file_path(file_name);
+
+	if(fs.existsSync(file_path)) throw "File " + file_name + " already exists in " + team_directory;
+	fs.writeFileSync(file_path, team_to_string(content));
 }
 
 /**
  * Reads a given team file and returns an array of the team information
  *
  * @param file_name Name of the file
+ * @throw error if file doesn't exist
  * @return array of team data as defined in Team.js
  */
 exports.read_team = function(file_name) {
-	//todo
-	return [];
+	var file_path = get_file_path(file_name);
+	if(!fs.existsSync(file_path)) throw "File " + file_name + " doesn't exist in " + team_directory;
+
+	var contents = fs.readFileSync(file_path, 'utf8');
+
+	return string_to_team(contents);
 }
 
 /**
@@ -85,19 +94,71 @@ exports.read_team = function(file_name) {
  *
  * @param file_name Name of the file
  * @param content Array represetnation of the team to write to file
+ * @throw error if file doesn't exist
  */
 exports.overwrite_team = function(file_name, content) {
-	//todo
-	return;
+	var file_path = get_file_path(file_name);
+
+	if(!fs.existsSync(file_path)) throw "File " + file_name + " doesn't exist in " + team_directory;
+	fs.writeFileSync(file_path, team_to_string(content));
 }
 
 /**
  * Returns an array of array representations of teams as defined in
  * the Team.js file
  *
- * @return array of teams as defined in Team.s
+ * @return array of teams as defined in Team.js
  */
 exports.get_all_teams = function() {
-	//todo
-	return [];
+	file_names = fs.readdirSync(team_directory);
+	teams = Array(file_names.length);
+
+	for(var el_no = 0; el_no < file_names.length; el_no++) {
+		teams[el_no] = exports.read_team(file_names[el_no].replace(".txt",""));
+	}
+
+	return teams;
+}
+
+function team_to_string(team_arr) {
+	var team_str = "";
+
+	for(var el_no = 0; el_no < team_arr.length; el_no++) {
+		if(util.isArray(team_arr[el_no])) {
+			// Sub Array -> String
+			team_str += "[[[" + team_arr[el_no].toString()
+											   .replace(/,/g,"/") + "]]]";
+		}
+		else {
+			// Field -> String
+			team_str += String(team_arr[el_no]);
+		}
+		if(el_no != team_arr.length - 1) team_str += ",";
+	}
+
+	return team_str;
+}
+
+function string_to_team(team_str) {
+	var team_arr = team_str.split(",");
+
+	for(el_no = 0; el_no < team_arr.length; el_no++) {
+		if(team_arr[el_no].substring(0,3) == "[[[") {
+			team_arr[el_no] = team_arr[el_no].replace('[[[','')
+											 .replace(']]]','')
+											 .split("/");
+		}
+	}
+
+	return team_arr;
+}
+
+
+
+exports.test_team_to_string = function(team_array) {
+	return team_to_string(team_array);
+}
+
+exports.test_string_to_team = function(team_string) {
+	return string_to_team(team_string);
 }
