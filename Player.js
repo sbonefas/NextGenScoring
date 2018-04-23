470c661386d2fbd101c22e467f0f39cf0de64374
Brett Wood <bwood@cs.wisc.edu>
2018-04-22 21:29:04 -0500

    Finished the teams page.  Users may now add/edit/delete teams as they wish

--- Player.js
+++ Player.js
@@ -6,24 +6,24 @@ module.exports = function(name, number, position, year){
 	this.number = number;
 	this.position = position;
 	this.year = year;
-	
+
 	this.get_name = function(){
 		return this.name;
 	}
-	
+
 	this.get_number = function(){
 		return this.number;
 	}
-	
+
 	this.get_position = function(){
 		return this.position;
 	}
-	
+
 	this.get_year = function(){
 		return this.year;
 	}
-	
+
 	this.to_array = function(){
-		return [this.name, this.number, this.position. this.year];
+		return [this.name, this.number, this.position, this.year];
 	}
 }
