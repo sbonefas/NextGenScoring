94cb808b1d47c09cce0f5cac971d06ea71f740d5
Noah Haselow <nhaselow@wisc.edu>
2018-04-23 23:27:16 -0500

    fixed get_all_- bug for real this time

--- data_read_write.js
+++ data_read_write.js
@@ -77,7 +77,7 @@ exports.edit_game_directory = function(new_path) {
 exports.get_all_games = function() {
 	file_names = fs.readdirSync(game_directory);
 	for(var i = 0; i < file_names.length; i++) {
-		if(file_names[i].substring(0,1) == '.' || file_names[i].slice(-4) == '.xml') {
+		if(file_names[i].substring(0,1) == '.' || file_names[i].slice(-4) != '.txt') {
 			file_names.splice(i, 1);
 			i--;
 		}
